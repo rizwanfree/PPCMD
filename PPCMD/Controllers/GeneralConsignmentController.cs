2a5c18fbc24a514c3fb44b08a4c6f96bb6e23182
rizwanfree <rizwanfreelance2022@gmail.com>
2025-10-02 19:18:29 +0500

    creating views

--- PPCMD/Controllers/GeneralConsignmentController.cs
+++ PPCMD/Controllers/GeneralConsignmentController.cs
@@ -1,12 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PPCMD.Data;
+using PPCMD.Models;
 
 namespace PPCMD.Controllers
 {
-    public class GeneralConsignmentController : Controller
+    public class GeneralConsignmentController : BaseController
     {
+
+        public GeneralConsignmentController(ApplicationDbContext context,UserManager<ApplicationUser> userManager) 
+            : base(context, userManager) { }
+
+
+        // GET: /GeneralConsignment/
         public IActionResult Index()
         {
-            return View();
+            var bls = _context.BLs
+                .Include(b => b.PendingBL)
+                .Include(b => b.LC)
+                .Include(b => b.Company)
+                .AsNoTracking()
+                .ToList();
+
+            return View(bls);
         }
     }
 }
